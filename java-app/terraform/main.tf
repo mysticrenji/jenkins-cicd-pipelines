346ec0a8b6e1a9fed95913768e3b63a7b65e9f99
Renjith Ravindranathan <mysticrenji@users.noreply.github.com>
2021-04-07 02:40:11 +0000

    Terraform update

--- java-app/terraform/main.tf
+++ java-app/terraform/main.tf
@@ -7,4 +7,7 @@ provider "azuread" {
 
 module "azureappservice-dev" {
   source = "./environments/dev"
+  dockerregistry = var.dockerregistry
+  dockerusername = var.dockerusername
+  dockerserverpassword = var.dockerserverpassword
 }
