87b1196d1f8d8146e6a004f817793aeffdf4c6ec
Renjith Ravindranathan <mysticrenji@users.noreply.github.com>
2021-04-12 05:58:07 +0000

    Added terraform doc

--- react-app/terraform/Terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.demo.kube_config.0.client_certificate
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.demo.kube_config_raw
-}
